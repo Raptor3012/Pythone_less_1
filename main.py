e8da42045a8881eb289e3c05be43297d3d15cde7
Raptor3012 <aleksey.shalov@gmail.com>
2020-11-13 13:58:16 +0500

    update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 
 class Sneakers(object):
 
@@ -20,11 +21,24 @@ class Storage(object):
     def Print(self):
         for obj in self.ListProduct:
             print(obj.Name, obj.Count, obj.Manufacturer, obj.Price, obj.Size)
+    
+    def LoadDataFromFile(self, filepath):
+        f = open(filepath, 'r')
+        for line in f:
+            inp = line.split(' ')
+            storage.AddProduct(Sneakers(inp[0],inp[1],inp[2],inp[3],inp[4]))
+        f.close()
+
+    def SaveDataFromFile(self):
+        f = open('./data/datasave.txt', 'w')
+        for obj in self.ListProduct:
+            f.writelines([obj.Name, ' ', obj.Count, ' ', obj.Manufacturer, ' ', obj.Price, ' ', obj.Size])
+        f.close()
 
 storage = Storage()
 
 while(True):
-    print("Выберете действие: '1' Добавить, '2' Покаазать склад ")
+    print("Выберете действие: \n'1':Добавить \n'2':Покаазать склад \n'3':Загрузить данные \n'4':Статистика \n'5':Выход ")
     inp = input()
     if inp == '1':
         print('Введите данные')
@@ -32,3 +46,12 @@ while(True):
         storage.AddProduct(Sneakers(inp[0],inp[1],inp[2],inp[3],inp[4]))    
     elif inp == '2':
         storage.Print()
+    elif inp == '3':
+        print("Введите путь к файлу:")
+        inp = input()
+        storage.LoadDataFromFile(inp)
+    elif inp == '4':
+        storage.Print()
+    elif inp == '5':
+        storage.SaveDataFromFile()
+        sys.exit()
